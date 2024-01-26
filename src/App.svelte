a99f9f9df21e4ff9bbaabc78af33969c8d77a5b9
lllilyk <ym0402kkk@naver.com>
2024-01-26 23:10:23 +0900

    논리 블록 {#if ...}

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,19 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+let auth = {
+  loggedIn: false
+}
+
+const handleLogin = () => auth.loggedIn = true;
+const handleLogout = () => auth.loggedIn = false;
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<button on:click={handleLogin} >Log In</button>
+<button on:click={handleLogout} >Log Out</button>
 
-  <div class="card">
-    <Counter />
-  </div>
+<p>{auth.loggedIn}</p>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+{#if auth.loggedIn === true}
+  <p>로그인 상태입니다.</p>
+{:else}
+  <p>로그아웃 상태입니다.</p>
+{/if}
