d651809790990ab09846a04f4a0ad3e4bacbeeb4
lllilyk <ym0402kkk@naver.com>
2024-01-26 23:43:27 +0900

    [slot] 마크업 영역을 효율적으로 재사용할 수 있게 도와줌

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
-  import Increment from './increment.svelte';
-  import Decrement from './decrement.svelte';
-  import CountResult from './countResult.svelte';
+  import Card from "./card.svelte";
 </script>
 
-<CountResult />
-<Increment />
-<Decrement />
+<Card>
+  <h2>왜 이렇게 매일 피곤할까요?</h2>
+  <p>날이 추워서 그런지 매일 전기장판 속에만 있고 싶어요.</p>
+</Card>
+
+<Card>
+  <h2>브러쉬업 라이프</h2>
+  <p>괴물과 한남자에서 인상깊게 본 배우의 드라마예요 재미있어요!</p>
+</Card>
