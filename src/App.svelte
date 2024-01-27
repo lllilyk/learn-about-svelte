cf6f89c9d286cbe55ef1ee190b3befce8eeb9814
lllilyk <ym0402kkk@naver.com>
2024-01-27 22:41:11 +0900

    [svelte:fragment] 필요없는 DOM 요소를 만들지 않고 바로 명명된 슬롯을 사용하는 방법

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,15 @@
 <script>
   import Card from "./card.svelte";
+  import Widget from "./widget.svelte";
 </script>
 
-<Card>
-  <span slot="name">
-    한교동
-  </span>
-
-  <span slot="address">
-    산리오시티<br>
-    헬로키티네 집
-  </span>
-</Card>
+<Widget>
+  <h1 slot="header">Hello</h1>
+  <svelte:fragment slot="footer">
+    <p>All rights reserved.</p>
+    <p>Copyright (c) 2019 Svelte Industries</p>
+  </svelte:fragment>
+</Widget>
 
 <br />
 
